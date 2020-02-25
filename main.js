7e9821655a1f73c825a79f7372efe2537a8343bc
VitalieVS <54939321+VitalieVS@users.noreply.github.com>
2020-02-25 12:26:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+class Calculator {
+    constructor() {
+        this.flag = false;
+        this.first = 0;
+        this.second = 0;
+        this.operator = '';
+    }
+
+    clearInput() {
+        this.operator = '';
+        this.first = 0;
+        this.second = 0;
+        this.flag = false;
+        input.value = '';
+    }
+
+    writeNumber(number) {
+        if (!this.flag) {
+            this.first += number;
+            input.value += number;
+        } else {
+            input.value += number;
+            this.second += number;
+        }
+        console.log("first", +this.first);
+        console.log("second= ", +this.second);
+    }
+
+    writeOperator(operator) {
+        this.operator = operator;
+        if (!this.flag && input.value.length !== 0) {
+            input.value += operator;
+            this.flag = true;
+        }
+    }
+
+    equal() {
+       console.log(parseInt(this.first) + parseInt(this.second));
+       console.log(this.operator);
+       input.value = '';
+       input.value = parseInt(this.first) + parseInt(this.second);
+       this.flag = false;
+       this.first = parseInt(this.first) + parseInt(this.second);
+       this.second = 0;
+    }
+}
+
+
+const input = document.querySelector(".calculator-screen");
+let calculator = new Calculator();
+
+const addListener = () => {
+    const keys = document.querySelectorAll("button");
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].addEventListener("click", () => {
+            if (keys[i].classList.contains("number")) {
+                calculator.writeNumber(keys[i].value);
+            } else if (keys[i].classList.contains("operator")) {
+                calculator.writeOperator(keys[i].value);
+            } else if (keys[i].classList.contains("all-clear")) {
+                calculator.clearInput();
+            }
+            else if (keys[i].classList.contains("equal-sign")){
+                calculator.equal();
+            }
+        });
+    }
+};
+
+addListener();
